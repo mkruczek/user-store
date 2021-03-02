27791e57afc69db1567e5b5b592fc07d65f010fd
mkruczek <kruczek86@gmail.com>
2021-03-02 07:33:41 +0100

    fix ut for preparing sql query

--- repository/user/user_test.go
+++ repository/user/user_test.go
@@ -11,9 +11,9 @@ func TestPrepareQuery(t *testing.T) {
 		values map[string][]string
 		want   string
 	}{
-		{map[string][]string{"first_name": {"jacek"}}, `SELECT id,first_name, last_name, email, create_date FROM users WHERE first_name='jacek'`},
-		{map[string][]string{"first_name": {"jacek", "andrzej"}}, `SELECT id,first_name, last_name, email, create_date FROM users WHERE first_name='jacek' OR first_name='andrzej'`},
-		{map[string][]string{"first_name": {"jacek", "andrzej"}, "last_name": {"placek"}}, `SELECT id,first_name, last_name, email, create_date FROM users WHERE first_name='jacek' OR first_name='andrzej' AND last_name='placek'`},
+		{map[string][]string{"first_name": {"jacek"}}, `SELECT id,first_name, last_name, email, create_date, update_date FROM users WHERE first_name='jacek'`},
+		{map[string][]string{"first_name": {"jacek", "andrzej"}}, `SELECT id,first_name, last_name, email, create_date, update_date FROM users WHERE first_name='jacek' OR first_name='andrzej'`},
+		{map[string][]string{"first_name": {"jacek", "andrzej"}, "last_name": {"placek"}}, `SELECT id,first_name, last_name, email, create_date, update_date FROM users WHERE first_name='jacek' OR first_name='andrzej' AND last_name='placek'`},
 	}
 
 	for _, c := range testCases {
